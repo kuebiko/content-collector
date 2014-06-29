90bf666e78ef14900c48d0d3e2c07178de4eb9bf
David Ramalho <dramalho@gmail.com>
2014-06-29 22:01:04 +0100

    Handle Tweet and Persona as a type of resource and save everything under the resources collection Handle Resource Relationships and partial resources

--- lib/content_collection_agent.rb
+++ lib/content_collection_agent.rb
@@ -1,30 +1,79 @@
-require 'mongo'
 require 'byebug'
+require 'mongo'
 
 class ContentCollectionAgent < Kuebiko::Agent
-  RESOURCE_CLASS = Kuebiko::MessagePayload::Document
-  DOCUMENT_TOPICS = {twitter: 'resources/twitter/tweet'}
+  RESOURCE_TOPICS = [
+    { topic: 'resources/twitter/tweet', klass: Kuebiko::MessagePayload::Document},
+    { topic: 'resources/persona',       klass: Kuebiko::MessagePayload::Persona }
+  ]
+
+  RELATIONSHIP_CLASS = Kuebiko::MessagePayload::ResourceRelationship
+  RELATIONSHIP_TOPIC = 'resources/relationship'
 
   def initialize
     super
 
     @mongo_client = Mongo::MongoClient.new.db('kuebiko')
 
-    DOCUMENT_TOPICS.each do |handler, topic|
+    # Documents
+    RESOURCE_TOPICS.each do |config|
       dispatcher.register_message_handler(
-        topic,
-        RESOURCE_CLASS,
-        method("handle_#{handler}".to_sym)
+        config[:topic],
+        config[:klass],
+        method(:handle_resource)
       )
     end
+
+    # Relationship
+    dispatcher.register_message_handler(
+      RELATIONSHIP_TOPIC,
+      RELATIONSHIP_CLASS,
+      method(:handle_relationship)
+    )
   end
 
-  def handle_twitter(msg)
+  def handle_resource(msg)
     hash = msg.payload.to_hash
     hash.each do |key, value|
       hash[key] = value.to_time.utc if value.is_a?(DateTime)
     end
 
-    @mongo_client.collection('twitter').insert(hash)
+    @mongo_client.collection('resources').update(
+      { type: msg.payload.type, source: msg.payload.source, source_id: msg.payload.source_id },
+      hash,
+      upsert: true
+    )
+  end
+
+  def handle_relationship(msg)
+    # Find resources
+    left_id = @mongo_client.collection('resources').find_one(
+      type: msg.payload.start_resource.type,
+      source: msg.payload.start_resource.source,
+      source_id: msg.payload.start_resource.source_id
+    )
+
+    left_id = left_id['_id'] if left_id
+
+    right_id = @mongo_client.collection('resources').find_one(
+      type: msg.payload.end_resource.type,
+      source: msg.payload.end_resource.source,
+      source_id: msg.payload.end_resource.source_id
+    )
+
+    right_id = right_id['_id'] if right_id
+
+    left_id = @mongo_client.collection('resources').insert(
+      msg.payload.start_resource.serialize.merge(version_type: :partial)
+    ) unless left_id
+
+    right_id = @mongo_client.collection('resources').insert(
+      msg.payload.end_resource.serialize.merge(version_type: :partial)
+    ) unless right_id
+
+    @mongo_client.collection('relationships').insert(
+      { left_resource_id: left_id, right_resource_id: right_id, type: msg.payload.type }
+    )
+
   end
 end
