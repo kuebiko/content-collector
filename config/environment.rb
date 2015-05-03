6bad7b9cbbb8027e6a5d11bb13f4271b03da8ec3
David Ramalho <dramalho@gmail.com>
2015-05-03 20:11:56 +0100

    tweaks and db: tasks

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,17 @@ require 'active_record'
 require 'yaml'
 require 'byebug'
 require 'kuebiko'
+require 'logger'
+
+ROOT_DIR = File.join(__dir__, '..')
 
 # recursively requires all files in ./lib and down that end in .rb
 Dir.glob('lib/*.rb').each do |file|
-  require_relative File.join(__dir__, '..', file)
+  require_relative File.join(ROOT_DIR, file)
 end
-
 
 database_config = YAML.load_file('config/database.yml')
 
 # tells AR what db file to use
+ActiveRecord::Base.logger = Logger.new(File.open( File.join(ROOT_DIR, 'log', 'database.log'), 'a'))
 ActiveRecord::Base.establish_connection(database_config['database'])
